d1b77d507a8835ec41ed951bf03d2fe2d97288da
jacek_roszko <jacek@Y50.localdomain>
2020-12-07 13:01:29 +0100

    sigature function refactored to html dom and works correctly now

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 (function signatureDraw() {
-    const canvas = $("#canvas");
-    const ctx = canvas[0].getContext("2d");
-    const signature = $("#signature");
+    const canvas = document.querySelector("#canvas");
+    const ctx = canvas.getContext("2d");
+    let signature = document.querySelector("#signature");
     let dataURL;
 
     // Attach a mousedown, mousemove, and mouseup event listener to the canvas DOM
@@ -9,24 +9,62 @@
     // on mousemove, continuously add a new point to the path with lineTo(), and color the last segment with stroke().
     // on mouseup, set a flag to disable the drawing.
 
-    canvas.on("mousedown", (evt) => {
+    canvas.addEventListener("mousedown", (evt) => {
         let flag = true;
-        let canvasX = canvas.offset().left;
-        let canvasY = canvas.offset().top;
+        // let canvasX = canvas.offset().left;
+        // let canvasY = canvas.offset().top;
+        let canvasX = canvas.getBoundingClientRect().left;
+        let canvasY = canvas.getBoundingClientRect().top;
         ctx.beginPath();
         ctx.lineWidth = 3;
         ctx.moveTo(evt.pageX - canvasX, evt.pageY - canvasY);
 
-        canvas.on("mousemove", (evt) => {
+        canvas.addEventListener("mousemove", (evt) => {
             if (flag) {
                 ctx.lineTo(evt.pageX - canvasX, evt.pageY - canvasY);
                 ctx.stroke();
             }
         });
-        $("canvas").on("mouseup", (evt) => {
+        canvas.addEventListener("mouseup", () => {
             flag = false;
-            // dataURL = canvas.val.toDataURL();
-            // signature = data.URL;
+            dataURL = canvas.toDataURL();
+            signature = dataURL;
+            console.log("singature: ", signature);
         });
     });
 })();
+
+// (function signatureDraw() {
+//     const canvas = $("#canvas");
+//     const ctx = canvas[0].getContext("2d");
+//     const signature = $("#signature");
+//     let dataURL;
+
+//     // Attach a mousedown, mousemove, and mouseup event listener to the canvas DOM
+//     // on mousedown, get the mouse coordinates, and use the moveTo() method to position your drawing cursor and the beginPath() method to begin a new drawing path.
+//     // on mousemove, continuously add a new point to the path with lineTo(), and color the last segment with stroke().
+//     // on mouseup, set a flag to disable the drawing.
+
+//     canvas.on("mousedown", (evt) => {
+//         let flag = true;
+//         let canvasX = canvas.offset().left;
+//         let canvasY = canvas.offset().top;
+//         ctx.beginPath();
+//         ctx.lineWidth = 3;
+//         ctx.moveTo(evt.pageX - canvasX, evt.pageY - canvasY);
+
+//         canvas.on("mousemove", (evt) => {
+//             if (flag) {
+//                 ctx.lineTo(evt.pageX - canvasX, evt.pageY - canvasY);
+//                 ctx.stroke();
+//             }
+//         });
+//         $("canvas").on("mouseup", () => {
+//             flag = false;
+//             // dataURL = canvas.toDataURL();
+//             dataURL = document.querySelector("#canvas").toDataURL();
+//             console.log(dataURL);
+//             // signature = data.URL;
+//         });
+//     });
+// })();
