ff0d8edac8f0491edd54757a287ae9ee8c7f5966
jacekroszkowiak <jacekroszkowiakdev@gmail.com>
2024-08-23 01:32:35 +0200

    chore(config): add .env file and environment variables

--- backend/configuration/config.js
+++ backend/configuration/config.js
@@ -1,11 +1,21 @@
+require("dotenv").config();
+dotenv.config();
+
+console.log("PORT:", process.env.PORT);
+
 const { Pool } = require("pg");
 
 const db = new Pool({
-    user: "petition_user",
-    host: "localhost",
-    database: "petitiondb",
-    password: "SuperSecret",
-    port: 5432,
+    // user: "petition_user",
+    // host: "localhost",
+    // database: "petitiondb",
+    // password: "SuperSecret",
+    // port: 5432,
+    user: USER,
+    host: HOST,
+    database: DATABASE,
+    password: PASSWORD,
+    port: PORT,
 });
 
 db.connect((err, client, release) => {
